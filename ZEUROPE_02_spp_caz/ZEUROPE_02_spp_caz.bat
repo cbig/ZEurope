4a55104c18e251dc694ac5331a8eb288f270f13e
Tuulit <tuuli.toivonen@helsinki.fi>
2014-01-27 22:31:39 +0200

    European Z analyses

---
+++ ZEUROPE_02_spp_caz/ZEUROPE_02_spp_caz.bat
@@ -0,0 +1,1 @@
+call zig4.exe -r Input/settings.dat Input/features_list.spp Output/ZEUROPE_02_spp_caz.txt 0.0 0 1.0 0  --grid-output-formats compressed-tif
---
+++ ZEUROPE_05_eu28_Natura_spp_abf/ZEUROPE_05_eu28_Natura_spp_abf.bat
@@ -0,0 +1,1 @@
+call zig4.exe -r Input/settings.dat Input/features_list.spp Output/ZEUROPE_05_eu28_Natura_spp_abf.txt 0.0 0 1.0 0  --grid-output-formats compressed-tif
